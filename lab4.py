3a510d0f86804d6c41f794d6d8f9f52d4e2ef98c
Alex361 <49017718+lazer361@users.noreply.github.com>
2024-10-08 16:18:48 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,161 +1,131 @@
 """
-С клавиатуры вводится два числа K и N. Квадратная матрица А(N,N), состоящая из 4-х равных по размерам подматриц, B,C,D,E
-заполняется случайным образом целыми числами в интервале [-10,10]. Для отладки использовать не случайное заполнение,
-а целенаправленное.
+С клавиатуры вводится два числа K и N. Квадратная матрица А(N,N), состоящая из 4-х равных по размерам подматриц,
+B,C,D,E заполняется случайным образом целыми числами в интервале [-10,10].
+Для отладки использовать не случайное заполнение, а целенаправленное.
 
 Вид матрицы А:
 Е	В
 D	С
 
-5 Вариант.	Формируется матрица F следующим образом: если в Е количество нулевых элементов в нечетных столбцах в области 4 больше,
-чем количество отрицательных элементов в четных строках в области 1, то поменять в В симметрично области 4 и 3 местами,
-иначе В и Е поменять местами несимметрично. При этом матрица А не меняется.
-После чего вычисляется выражение: ((F+A)– (K * F) )*AT .
+Для простоты все индексы в подматрицах относительные.
+По сформированной матрице F (или ее частям) необходимо вывести не менее 3 разных графиков.
+Программа должна использовать функции библиотек numpy  и mathplotlib
+
+
+5 Вариант.	Формируется матрица F следующим образом: скопировать в нее А и если А симметрична относительно главной
+диагонали, то поменять местами С и  В симметрично, иначе С и Е поменять местами несимметрично.
+При этом матрица А не меняется.
+После чего если определитель матрицы А больше суммы диагональных элементов матрицы F, то вычисляется выражение:
+A-1*AT – K * F-1, иначе вычисляется выражение (AТ +G-FТ)*K, где G-нижняя треугольная матрица, полученная из А.
 Выводятся по мере формирования А, F и все матричные операции последовательно.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pylab
+import random
 
-K = int(input("Введите число K:"))
-N = int(input("Введите число K:"))
-M = N // 2
-count_zero_elements = 0  # Кол-во нулевых элементов
-count_negative_elements = 0  # кол-во отрицательных элементов
+# Ввод размеров матрицы
+K = int(input("Введите K: "))
+N = int(input("Введите N: "))
 
-print("B")
-b = np.random.randint(-10, 10, (M, M))
-print(b, '\n')
-print("C")
-c = np.random.randint(-10, 10, (M, M))
-print(c, '\n')
-print("D")
-d = np.random.randint(-10, 10, (M, M))
-print(d, '\n')
-print("E")
-e = np.random.randint(-10, 10, (M, M))
-print(e, '\n')
+# Проверка на четность N
+if N % 2 != 0:
+    print("N должно быть четным числом!")
+    exit()
 
-# Матрица А
-print("Матрица A: ")
-a = np.vstack(((np.hstack([d, e])), (np.hstack([c, b]))))
-print(a)
+# Размер подматриц
+submatrix_size = N // 2
 
-print()
-#  **** Определитель матрицы A
-det_A = int(np.linalg.det(a))
-print(f"Определитель матрицы A: {det_A}")
+# Целенаправленное заполнение подматриц
+# Для проверки используйте следующие значения:
+# E = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+# B = [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]]
+# D = [[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]]
+# C = [[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]
 
-# G-нижняя треугольная матрица, полученная из А
-g = np.tri(N) * a
-print("Матрица G:")
-print(g)
+# E = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+# B = [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]]
+# D = [[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]]
+# C = [[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]
 
-# Матрица F
-f = a.copy()
-print("матрица F")
-print(f)
+E = np.array([[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)])
+B = np.array([[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)])
+D = np.array([[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)])
+C = np.array([[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)])
 
-# Кол-во нулевых элементов в матрице E
-for i in range(M):
-    for j in range(0, M, 2):
-        if e[i][j] == 0:
-            count_zero_elements += 1
-print(f"Кол-во нулевых элементов в матрице E: {count_zero_elements}")
+# Создание матрицы A
+A = np.concatenate((np.concatenate((E, B), axis=1), np.concatenate((D, C), axis=1)), axis=0)
+print("Матрица A:")
+print(A)
 
+# Создание матрицы F
+F = A.copy()
+print("Матрица F:")
+print(F)
 
-# количество отрицательных элементов в матрице E
-for i in range(1, M, 2):
-    for j in range(0, M):
-        if e[i][j] < 0:
-            count_negative_elements += 1
-print(f"Количество отрицательных элементов в матрице E: {count_negative_elements}")
+# Проверка на симметричность
+symmetric = np.allclose(A, A.T)
 
-
-if count_zero_elements >= count_negative_elements:
-    print("Кол-во нулевых элементов в нечёт.стлбц больше, чем кол-во отрицательных элементов в чётных строках")
-    # меняем C и B симметрично
-    f[M:N, M:N] = np.fliplr(c)
-    f[M:N, :M] = np.fliplr(b)
+# Изменение областей в F
+if symmetric:
+    print("Обмен областей C и B симметрично")
+    # Обмен областей C и B симметрично
+    F[:submatrix_size, submatrix_size:] = F[submatrix_size:, :submatrix_size].copy()
+    F[submatrix_size:, :submatrix_size] = A[:submatrix_size, submatrix_size:].copy()
 else:
-    # меняем C и B несимметрично
-    print("Кол-во нулевых элементов в нечёт.стлбц меньше или равно, чем кол-во отрицательных элементов в чётных строках")
-    f = np.vstack(((np.hstack([d, b])), (np.hstack([c, e]))))
+    print("Обмен областей C и E несимметрично")
+    # Обмен областей C и E несимметрично
+    F[submatrix_size:, :submatrix_size] = F[:submatrix_size, :submatrix_size].copy()
+    F[:submatrix_size, :submatrix_size] = A[submatrix_size:, :submatrix_size].copy()
 
+print("Матрица F после преобразования:")
+print(F)
 
-# *** Сумма Диагональных элементов
-summ_diagonal_elements = sum(np.diagonal(f))
-print("Сумма Диагональных элементов:", summ_diagonal_elements)
+# Вычисление определителя матрицы A
+det_A = np.linalg.det(A)
+print("Определитель матрицы A:", det_A)
 
-if det_A > summ_diagonal_elements:
-    print("Определитель Матрицы А больше суммы диагональных элементов матрицы F")
-    print("A-1")
-    print(np.linalg.inv(a))
-    print()
-    print("A_T")
-    print(np.matrix.transpose(a))
-    print()
-    print("K * F")
-    print(K * f)
-    print()
-    print("A-1 * A_T")
-    print((np.linalg.inv(a) * np.matrix.transpose(a)))
-    print()
-    print("A-1 * A_T - K * F")
-    rezult = (np.linalg.inv(a) * np.matrix.transpose(a)) - (K * f)
-    print(rezult)
+# Вычисление суммы диагональных элементов матрицы F
+sum_diag_F = np.trace(F)
+print("Сумма диагональных элементов матрицы F:", sum_diag_F)
+AT = A.T
+# Выбор ветви вычислений
+if det_A > sum_diag_F:
+    print("det_A > sum_diag_F")
+    # Вычисление A-1*AT – K * F-1
+    A_inverse = np.linalg.inv(A)
+    result = np.dot(A_inverse, AT) - K * np.linalg.inv(F)
 else:
-    print("Определитель Матрицы А меньше или равно суммы диагональных элементов матрицы F")
-    print("A_T")
-    print(np.matrix.transpose(a))
-    print()
-    print("F-1")
-    print(np.linalg.inv(f))
-    print()
-    print("G - F-1")
-    print(g - np.linalg.inv(f))
-    print()
-    print("A_T + G - F-1")
-    print(np.matrix.transpose(a) + g - np.linalg.inv(f))
-    print()
-    print("(A_T + G - F-1) * K")
-    rezult = (np.matrix.transpose(a) + g - np.linalg.inv(f)) * K
-    print(rezult)
+    print("det_A <= sum_diag_F")
+    # Вычисление (AТ +G-FТ)*K
+    G = np.tril(A) # Нижняя треугольная матрица
+    FT = F.T
+    result = np.dot(AT + G - FT, K)
 
-F3 = f.reshape(-1)
-x = list()
-for i in range(N):
-    for j in range(N):
-        x.append(str(i)+str(j))
-pylab.subplot(2, 2, 1)
-pylab.plot(x, F3, marker='o', markersize=3)
-pylab.xlabel('Индекс элемента')
-pylab.ylabel('Значение')
-pylab.title('Первый график')
+# Вывод результата
+print("Результат:")
+print(result)
 
-pylab.subplot(2, 2, 2)
-F4 = np.sort(f, axis=None)
+# Построение графиков
+plt.figure(figsize=(12, 4))
 
-pylab.plot(x, F4, color='red', marker='o', markersize=3)
-pylab.xlabel('Индекс элемента')
-pylab.ylabel('Значение')
-pylab.title('Второй график')
+# График 1: Гистограмма
+plt.subplot(2, 2, 1)
+plt.hist(result.flatten(), bins=10)
+plt.title('Гистограмма значений результата')
 
+# График 2: Круговая диаграмма
+plt.subplot(2, 2, 2)
+unique, counts = np.unique(result.flatten(), return_counts=True)
+plt.pie(counts, labels=unique, autopct='%1.1f%%')
+plt.title('Круговая диаграмма значений результата')
 
-pylab.subplot(2, 2, 3)
-F6 = np.max(f, axis=1)
-pylab.bar(np.arange(N),F6, color='green')
-pylab.xlabel('Номер строки')
-pylab.ylabel('Max')
-pylab.title('Третий график')
+# График 3: Линейная диаграмма
+plt.subplot(2, 2, 3)
+plt.plot(result.flatten())
+plt.title('Линейная диаграмма значений результата')
+plt.xlabel('Индекс')
+plt.ylabel('Значение')
 
-pylab.subplot(2, 2, 4)
-F7 = np.min(f, axis=1)
-pylab.bar(np.arange(N), F7, color='orange')
-pylab.plot(np.arange(N), F7, color='purple', marker='o', markersize=3)
-pylab.xlabel('Номер строки')
-pylab.ylabel('Min')
-pylab.title('Четвертый график')
-
+plt.tight_layout()
 plt.show()
