80184e7ad7d3014ec00591a2feabc9398a7e01b3
Alex361 <49017718+lazer361@users.noreply.github.com>
2024-12-19 16:20:13 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,150 +1,176 @@
-"""
-С клавиатуры вводится два числа K и N. Квадратная матрица А(N,N), состоящая из 4-х равных по размерам подматриц, B,C,D,E
-заполняется случайным образом целыми числами в интервале [-10,10].
+'''
+С клавиатуры вводится два числа K и N.
+Квадратная матрица А(N,N), состоящая из 4-х равных по размерам подматриц, B,C,D,E заполняется случайным образом целыми числами в интервале [-10,10].
 Для тестирования использовать не случайное заполнение, а целенаправленное.
 
-Вид матрицы А:
-Е	В
-D	С
-
-Для простоты все индексы в подматрицах относительные. Библиотечными методами пользоваться нельзя.
-
-
-5 Вариант.	Формируется матрица F следующим образом: если А симметрична относительно главной диагонали,
+Вариант 5
+5.	Формируется матрица F следующим образом: если А симметрична относительно главной диагонали,
 то поменять в В симметрично области 1 и 3 местами, иначе С и Е поменять местами несимметрично.
-При этом матрица А не меняется. После чего вычисляется выражение: К * (F+A) * AT – AT + F.
+При этом матрица А не меняется. После чего вычисляется выражение: К * (F+A) * A^T – A^T + F.
 Выводятся по мере формирования А, F и все матричные операции последовательно.
-"""
+Вид матрицы A:
+E B
+D C
+
+Каждая из матриц B,C,D,E имеет вид:    1
+                                     4   2
+                                       3
+'''
 
 import random
 
-# Ввод размеров матрицы
-K = int(input("Введите K: "))
+
+# Функция вывода матриц
+def print_matrix(matrix):
+    for row in matrix:
+        print("|", end='')
+        for element in row:
+            print("{:3}".format(element), end=' ')
+        print("|")
+
+
+def check_matrix_A(matrix):
+    for row in range(N):
+        for col in range(row + 1, N):
+            if matrix[row][col] != matrix[col][row]:
+                return False
+    return True
+
+
+# Транспонирование матрицы
+def transpose_matrix(matrix, len_matrix):
+    # Создаем новую пустую матрицу для хранения результата
+    transposed = [[0 for _ in range(len_matrix)] for _ in range(len_matrix)]
+    # Транспонируем матрицу
+    for i in range(len_matrix):
+        for j in range(len_matrix):
+            transposed[j][i] = matrix[i][j]
+    return transposed
+
+
+# ================================ №1 Создание основной матрицы A =========================================
+# Вводим значения K и N с клавиатуры
+K = int(input("Введите размер K: "))
 
 while True:
-    N = int(input("Введите размер/ранг матрицы (N): "))
-    if N >= 6 and N % 2 == 0:
+    N = int(input("Введите размер матрицы N: "))
+    if 6 <= N <= 50:
         break
     else:
-        print(
-            "Пожалуйста, введите размер/ранг матрицы (N) число больше/равно 6 и четное число для равных по размеру подматриц")
+        print("Ошибка: Размер матрицы должен быть не меньше 6 и не больше 50.")
 
-# Размер подматриц
-submatrix_size = N // 2
+# Создаем пустую матрицу A(N, N)
+matrix_A = [[0 for _ in range(N)] for _ in range(N)]
 
-# Целенаправленное заполнение подматриц
-# Для проверки используйте следующие значения:
-# E = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-# B = [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]]
-# D = [[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]]
-# C = [[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]
+# Заполняем матрицу A(N, N) случайными числами
+for row in range(N):
+    for column in range(N):
+        matrix_A[row][column] = random.randint(-10, 10)
 
-# E = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-# B = [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]]
-# D = [[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]]
-# C = [[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]
+# Заполняем матрицу для тестирования, размер 10x10. Случай с симметричной матрицой А
+K = 5
+N = 10
+matrix_A = [
+    [9, 9, 9, 9, 9, 9, 0, 0, 0, 0],
+    [10, 9, 9, 9, 2, 1, 1, 1, 1, 1],
+    [9, 9, 9, 9, 2, 2, 2, 2, 2, 2],
+    [9, 9, 9, 9, 2, 3, 3, 3, 3, 3],
+    [9, 2, 2, 2, 2, 4, 4, 4, 4, 4],
+    [9, 1, 2, 3, 4, 4, 4, 4, 4, 4],
+    [0, 1, 2, 3, 4, 4, 3, 3, 3, 3],
+    [0, 1, 2, 3, 4, 4, 3, 2, 2, 2],
+    [0, 1, 2, 3, 4, 4, 3, 2, 1, 1],
+    [0, 1, 2, 3, 4, 4, 3, 2, 1, 1]
+]
 
-E = [[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)]
-B = [[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)]
-D = [[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)]
-C = [[random.randint(-10, 10) for _ in range(submatrix_size)] for _ in range(submatrix_size)]
+# Определяем размер каждой подматрицы
+SIZE_submat = N // 2
 
-# Создание матрицы A
-A = [E[i] + B[i] for i in range(submatrix_size)] + [D[i] + C[i] for i in range(submatrix_size)]
-print("Матрица A:")
-for row in A:
-    print(row)
+# Выводим матрицу A
+print("\nМатрица A(N, N):")
+print_matrix(matrix_A)
 
-# Проверка на симметричность
-symmetric = True
-for i in range(N):
-    for j in range(i + 1, N):
-        if A[i][j] != A[j][i]:
-            symmetric = False
-            break
-    if not symmetric:
-        break
+# Создаем и заполняем матрицу F
+matrix_F = [[item for item in row] for row in matrix_A]
 
-# Формирование матрицы F
-F = [E[i] + B[i] for i in range(submatrix_size)] + [D[i] + C[i] for i in range(submatrix_size)]
-print("Матрица F:")
-for row in F:
-    print(row)
+# Проверка на симметричность матрицы А
+if check_matrix_A(matrix_A):
+    print("\nМатрица A: симметрична")
+    # Создаем и заполняем подматрицу B
+    matrix_B = [[0 for _ in range(SIZE_submat)] for _ in range(SIZE_submat)]
+    for row in range(SIZE_submat):
+        matrix_B[row] = matrix_A[row][SIZE_submat + N % 2:]
+    print("\nМатрица B:")
+    print_matrix(matrix_B)
 
-# Изменение областей в F
-if symmetric:
-    print("Обмен областей 1 и 3 в B симметрично")
-    # Обмен областей 1 и 3 в B симметрично
-    for i in range(submatrix_size):
-        for j in range(submatrix_size):
-            temp = F[i][j]
-            F[i][j] = F[i + submatrix_size][j + submatrix_size]
-            F[i + submatrix_size][j + submatrix_size] = temp
+    # меняем в В симметрично области 1 и 3 местами
+    for row in range(SIZE_submat):
+        for column in range(row, SIZE_submat-row):
+            matrix_B[row][column], matrix_B[SIZE_submat-1-row][column] = matrix_B[SIZE_submat-1-row][column], matrix_B[row][column]
+
+    print("\nМатрица B после замены областей 1 и 3:")
+    print_matrix(matrix_B)
+    for row in range(SIZE_submat):
+        matrix_F[row][SIZE_submat + N % 2:] = matrix_B[row]
+
 else:
-    print("Обмен областей C и E несимметрично")
-    # Обмен областей C и E несимметрично
-    for i in range(submatrix_size):
-        for j in range(submatrix_size):
-            temp = F[i][j]
-            F[i][j] = F[i + submatrix_size][j]
-            F[i + submatrix_size][j] = temp
+    print("\nМатрица A: несимметрична")
+    # меняем С и Е местами несимметрично
+    for row in range(SIZE_submat):
+            matrix_F[row][:SIZE_submat], matrix_F[row + SIZE_submat + N % 2][SIZE_submat + N % 2:] = \
+                matrix_F[row + SIZE_submat + N % 2][SIZE_submat + N % 2:], matrix_F[row][:SIZE_submat]
 
-print("Матрица F после преобразования:")
-for row in F:
-    print(row)
+print("\nМатрица F после всех изменений: ")
+print_matrix(matrix_F)
 
-# Сложение матриц F и A
-FA = [[0 for _ in range(N)] for _ in range(N)]
-print("F + A:")
-for i in range(N):
-    for j in range(N):
-        FA[i][j] = F[i][j] + A[i][j]
-        print(FA[i][j], end=" ")
-    print()
 
-# Транспонирование матрицы A
-AT = [[0 for _ in range(N)] for _ in range(N)]
-print("Транспонированная матрица A (AT):")
-for i in range(N):
-    for j in range(N):
-        AT[j][i] = A[i][j]
-        print(AT[i][j], end=" ")
-    print()
+# ================================ №2 Матричные операции =========================================
+# К * (F+A) * A^T – A^T + F
+print("\nСумма F+A: ")
+sum_FA = [[0 for _ in range(N)] for _ in range(N)]
+for row in range(N):
+    for column in range(N):
+        sum_FA[row][column] = matrix_A[row][column] * matrix_F[row][column]
+print_matrix(sum_FA)
 
-# Умножение матрицы (F+A) на AT
-result = [[0 for _ in range(N)] for _ in range(N)]
-print("(F + A) * AT:")
-for i in range(N):
-    for j in range(N):
+print("\nПроизведение K * (F + A): ")
+mult_KAF = [[0 for _ in range(N)] for _ in range(N)]
+for row in range(N):
+    for column in range(N):
+        mult_KAF[row][column] = sum_FA[row][column] * K
+print_matrix(mult_KAF)
+
+print("\nТранспонирование матрицы (A^T): ")
+trans_A = transpose_matrix(matrix_A, N)
+print_matrix(trans_A)
+
+print("\nПроизведение К * (F+A) * A^T: ")
+mult_KAF_AT = [[0 for _ in range(N)] for _ in range(N)]
+for row in range(N):
+    for column in range(N):
         for k in range(N):
-            result[i][j] += FA[i][k] * AT[k][j]
-        print(result[i][j], end=" ")
-    print()
+            mult_KAF_AT[row][column] = mult_KAF[row][k] * trans_A[k][column]
+print_matrix(mult_KAF_AT)
 
-# Умножение на K
-print("K * (F+A) * AT:")
-for i in range(N):
-    for j in range(N):
-        result[i][j] *= K
-        print(result[i][j], end=" ")
-    print()
 
-# Вычитание AT
-print("- AT:")
-for i in range(N):
-    for j in range(N):
-        result[i][j] -= AT[i][j]
-        print(result[i][j], end=" ")
-    print()
+print("\nРазность матриц К * (F+A) * A^T – A^T: ")
+dif_matrix = [[0 for _ in range(N)] for _ in range(N)]
+for row in range(N):
+    for column in range(N):
+        dif_matrix[row][column] = mult_KAF_AT[row][column] - trans_A[row][column]
+print_matrix(dif_matrix)
 
-# Сложение с F
-print("+ F:")
-for i in range(N):
-    for j in range(N):
-        result[i][j] += F[i][j]
-        print(result[i][j], end=" ")
-    print()
 
-print("Результат: К * (F+A) * AT – AT + F:")
-for row in result:
-    print(row)
+print("\nИтоговый результат К * (F+A) * A^T – A^T + F: ")
+end_result = [[0 for _ in range(N)] for _ in range(N)]
+for row in range(N):
+    for column in range(N):
+        end_result[row][column] = dif_matrix[row][column] + matrix_F[row][column]
+
+for row in end_result:
+    print("|", end='')
+    for element in row:
+        print("{:5}".format(element), end=' ')
+    print("|")
+print("")
+
